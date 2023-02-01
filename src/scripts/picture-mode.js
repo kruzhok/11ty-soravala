6e840265d4417a9f0ceeae01daa201918b303f7d
pa7lux <pa7lux@gmail.com>
2023-02-01 19:07:44 +0400

    linked ckeckboxes

--- src/scripts/picture-mode.js
+++ src/scripts/picture-mode.js
@@ -1,16 +1,31 @@
-const tumbler  = document.querySelector('.switch-checkbox-2 .checkbox-2')
+const tumbler  = document.querySelector('.switch-checkbox .checkbox')
 const photos = document.querySelectorAll('.grid-list .image-small')
-
-
-setImageStateAndTurnItOn(tumbler, photos, 'image-small--fantasy', 'image-small--reality', 'hidden-image')
-
-
 const detailTumbler = document.querySelector('#switch-detail');
 const photosDetail = document.querySelectorAll('.page-image');
+const gridTumbler = document.querySelector('#switch-2');
+const tumblerMobile = document.querySelector('#switch-3');
 
-setImageStateAndTurnItOn(detailTumbler, photosDetail, 'page-image--fantasy', 'page-image--real', 'hidden-image')
+function setImageSmall(tumbler, PhotoName) {
+  setImageStateAndTurnItOn(tumbler, PhotoName, 'image-small--fantasy', 'image-small--reality', 'hidden-image')
+}
+function setPageImage(tumbler, PhotoName) {
+  setImageStateAndTurnItOn(tumbler, PhotoName, 'page-image--fantasy', 'page-image--real', 'hidden-image')
+}
+setImageSmall(tumbler, photos)
+setImageSmall(tumbler, document.querySelectorAll('.backgr-image'))
+setImageSmall(gridTumbler, photos)
+setImageSmall(tumblerMobile, photos)
+setImageSmall(tumblerMobile, document.querySelectorAll('.backgr-image'))
+setPageImage(tumblerMobile, photosDetail)
+setPageImage(tumbler, photosDetail)
+setPageImage(detailTumbler, photosDetail)
 
 
+tumbler.checked && [
+  toggleImage(photos, tumbler.checked, 'image-small--fantasy', 'image-small--reality', 'hidden-image'),
+  toggleImage(photosDetail, tumbler.checked, 'page-image--fantasy', 'page-image--real', 'hidden-image'),
+  toggleImage(document.querySelectorAll('.backgr-image'), tumbler.checked, 'image-small--fantasy', 'image-small--reality', 'hidden-image'),
+]
 
 function setImageStateAndTurnItOn(tumblerName, photosToChange, fantasyPhotoClassName, realPhotoClassName, toggleClassName) {
   let tumbler = tumblerName;
---
+++ src/scripts/save-checkbox.js
@@ -0,0 +1,33 @@
+const checkbox  = document.querySelector('.switch-checkbox .checkbox');
+const detailCheckbox = document.querySelector('#switch-detail');
+const gridCheckbox = document.querySelector('#switch-2');
+const checkboxMobile = document.querySelector('#switch-3');
+const checkboxDB = [checkbox, gridCheckbox, checkboxMobile, detailCheckbox]
+
+checkbox.addEventListener('change', function() {
+  checkboxDB.forEach(item => {
+    if(item) {
+      item.checked = checkbox.checked;
+    }
+  })
+})
+
+function checkboxEventListener(checkbox) {
+    checkbox.addEventListener('change', function() {
+        localStorage.setItem('checkboxValue', this.checked);
+    });
+}
+
+function checkboxChecked(checkbox) {
+    if (checkbox) {
+        checkbox.checked = JSON.parse(localStorage.getItem('checkboxValue'));
+    }
+}
+
+let i = 0
+for(i; i < checkboxDB.length; i++) {
+    checkboxChecked(checkboxDB[i])
+}
+
+checkboxEventListener(checkbox)
+checkboxEventListener(checkboxMobile)
