c5781f9e2364d3f094f4228ecc0ea0238eaa5efd
pa7lux <pa7lux@gmail.com>
2023-02-06 16:03:21 +0400

    в jquery скрипте ожидаю загрузки всех элементров  на странице, чтобы не было ошибки в консоли

--- src/scripts/mobile-menu.js
+++ src/scripts/mobile-menu.js
@@ -1,14 +1,16 @@
-$(function() {
-    function doAllTheThingsToToggleMenu() {
-        $('#mobile-menu').toggleClass('menu-open')
-        $('#background').toggleClass('menu-open')
-        $('#body').toggleClass('menu-open')
-        $('#menu-toggle').toggleClass('menu-open')
-        $('#close-menu-button').toggleClass('menu-open')
-    }
+$(document).ready(function() {
+  function doAllTheThingsToToggleMenu() {
+    $('#mobile-menu').toggleClass('menu-open');
+    $('#background').toggleClass('menu-open');
+    $('#body').toggleClass('menu-open');
+    $('#menu-toggle').toggleClass('menu-open');
+    $('#close-menu-button').toggleClass('menu-open');
+  }
 
-    $('#menu-toggle').on('click', doAllTheThingsToToggleMenu)
-    $('#background').on('click', doAllTheThingsToToggleMenu)
-    $('#close-menu-button').on('click', doAllTheThingsToToggleMenu)
-})(jQuery)
+  $('#menu-toggle').on('click', doAllTheThingsToToggleMenu);
+  $('#background').on('click', doAllTheThingsToToggleMenu);
+  $('#close-menu-button').on('click', doAllTheThingsToToggleMenu);
+})
+
+
 
