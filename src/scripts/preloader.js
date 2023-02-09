f869e7cf60fa00709a7f592e0731058d06469e4b
pa7lux <pa7lux@gmail.com>
2023-02-09 15:28:04 +0400

    state of preloader in sessionStorage

--- src/scripts/preloader.js
+++ src/scripts/preloader.js
@@ -8,24 +8,35 @@ function delay(ms) {
   });
 }
 
-delay(100)
+if(sessionStorage.getItem("preloaderPlayed")) {
+  preloader.style.display = 'none'
+  delay(300)
+    .then(()=>root.classList.add('fadedIn'))
+} else {
+  delay(100)
   .then(()=>preloader.classList.add('fadedIn'))
 
-delay(2000)
-  .then(()=>preloaderTitle.classList.add('fadedIn'))
+  delay(2000)
+    .then(()=>preloaderTitle.classList.add('fadedIn'))
 
-delay(3000)
-.then(()=>{
-  preloader.classList.remove('fadedIn');
-  preloader.classList.add('fadedOut');
+  delay(3000)
+    .then(()=>{
+      preloader.classList.remove('fadedIn');
+      preloader.classList.add('fadedOut');
+    }
+  )
+
+  delay(3000)
+    .then(()=>root.classList.add('fadedIn'))
+
+  delay(6000)
+    .then(()=>preloader.style.display = 'none')
+    .then(()=>{
+      sessionStorage.setItem("preloaderPlayed", true);
+    })
 }
-)
 
-delay(3000)
-  .then(()=>root.classList.add('fadedIn'))
 
-delay(6000)
-  .then(()=>preloader.style.display = 'none')
 
 
 
